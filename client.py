c4465a53a7c35a7423284d2e100b21f3bb5bed6b
xubin1986 <elverken@163.com>
2018-02-11 04:26:51 +0800

    it works at this version

--- client.py
+++ client.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 import socket,sys,commands,json,re,time,signal,ConfigParser,os
-is_exit = 0
 aepath = '/root/code/aiui_devops'
 sys.path.append(aepath)
 from ae_service import *
@@ -20,6 +19,7 @@ def signal_handler(signum, frame):
     is_exit = 1
 
 def start():
+    global is_exit
     print '\nStarting AE client...',
     if Readconfig('agent','agent') == 'yes':
         section = 'agent'
@@ -28,7 +28,7 @@ def start():
     remote_ip_port = (Readconfig(section,'host'),int(Readconfig(section,'port')))
     local_ip_port = ("0.0.0.0",int(Readconfig('client','port')))
     sk = socket.socket()
-    sk.bind(local_ip_port)
+    #sk.bind(local_ip_port)
     try:
         sk.connect(remote_ip_port)
         pid.write(pid_file)
@@ -39,19 +39,14 @@ def start():
         log('debug','Failed to conenct server %s from %s as reason %s' % (remote_ip_port,local_ip_port,str(e)))
         sys.exit(1)
     while True:
-        try:
-            data = sk.recv(1024)
-            print data
-            if data == 'exit':
-                is_exit = '1'
-        except:
-            pass
+        data = sk.recv(1024)
+        if data == 'yes': is_exit = 1
         data0 = json.dumps(getData())
         sk.sendall(data0)
         time.sleep(1)
         if is_exit == 1:
             sk.sendall('exit')
-            time.sleep(1)
+            time.sleep(0.5)
             break
     sk.close()
 
@@ -62,7 +57,7 @@ def stop():
     pid.remove(pid_file)
     print 'Done!'
    
-    
+is_exit = 0   
 signal.signal(signal.SIGALRM, signal_handler)
 try:
     cmd = sys.argv[1]
--- server.py
+++ server.py
@@ -5,31 +5,29 @@ import sys
 class MyServer(SocketServer.BaseRequestHandler):
     def handle(self):
         conn = self.request
-        conn.sendall('Welcome to join into AE!')
         client = str(conn.getpeername())
         while True:
-            print client_exit
             if client_exit == 1:
-                conn.sendall('exit')
+                conn.sendall('yes')
+            elif client_exit == 2:
+                conn.close()
+            else:
+                conn.sendall('no')
             data = conn.recv(1024)
             print client + ' : ' + data
             if data == 'exit':
                 print 'Client %s is leaving' % client
                 break
-            time.sleep(1)
-
-def signal_handler(signum, frame):
-    global client_exit
-    client_exit = 1
+        conn.close()
+        
 client_exit = 0
-signal.signal(signal.SIGALRM, signal_handler)
 server = SocketServer.ThreadingTCPServer(('0.0.0.0',8002),MyServer)
-server.serve_forever()
-# try:
-    # server.serve_forever()
-# except KeyboardInterrupt:
-    # print 'exit'
-# finally:
-    # client_exit = 1
-    # time.sleep(5)
-    # server.server_close()
+try:
+    server.serve_forever()
+except KeyboardInterrupt:
+    print 'exit'
+finally:
+    client_exit = 1
+    time.sleep(5)
+    #need check if exist client, and close them by set client_exit = 2
+    server.server_close()
